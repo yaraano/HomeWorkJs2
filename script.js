54b1f76503d25aae8f15bcdd1dbc2a62471dea36
yaraano <qwertystar24@gmail.com>
2023-10-27 03:48:09 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+// Hello ivan
+
+// let a = 'hello'
+// let b = 'ivan'
+// let c = a.slice(0,1).toUpperCase() + a.slice(1) + ' ' + b.slice(0,1).toUpperCase() + b.slice(1)
+// console.log(c)
+
+
+
+// записать длину в переменную b
+
+// let a = 'lorem ipsum dolor'
+// let b = a.length
+// console.log(b)
+
+
+
+// вернуть sit lorem ipsum dolor amet
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.substring(18,21) + " " + a.substring(0, 17) +  " " + a.substring(22)
+// console.log(b)
+
+
+
+//вернуть CHILDREn
+
+// let a = "children";
+// let result = a.slice(0,-1).toUpperCase() + a.slice(-1).toLowerCase();
+// console.log(result)
+
+
+
+//  вернуть bsso,
+
+// let a = 'boss'
+// console.log(a.slice(0,1) + a.slice(2) + a.slice(1,2))
+
+
+
+// вернуть index d
+
+// let a = 'sunday'
+// console.log(a.indexOf('d'))
+
+
+
+// вернуть hekso
+
+// let a = 'hello';
+// console.log(a.slice(0, 2) + 'ks' + a.slice(4))
+
+
+
+// вернуть new us
+
+// let a = 'user'
+// let b = 'new'
+
+// console.log(b + ' ' + a.slice(0,2))
+
+
+
+// вернуть daydaymon
+
+// let a = 'monday'
+// let b = 'day'
+//
+// console.log(b + b + a.slice(0,3))
+
+
+// вернуть 'hEILo'
+
+// let a = 'HELLO'
+//
+// console.log(a.slice(0,1).toLowerCase() +  a.slice(1,2) + 'i'.toUpperCase() + a.slice(3,4) + a.slice(-1).toLowerCase())
+
+
+// вернуть '450'
+
+// let a = 5
+// let b = '400'
+// console.log(b.slice(0,1) + a + b.slice(1,2))
+
+
+
+// вернуть manak
+//
+// let a = 'monkey'
+// let b = 'banana'
+//
+// console.log(a[0] + b[1] + a[2] + b[1] + a[3])
